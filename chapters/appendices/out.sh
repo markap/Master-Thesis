1f68be0850e6755aa773fadaa64a88e4d52e2fc9
Martin <mart.kapfhammer@gmail.com>
2015-03-29 20:10:18 +0200

    finalize

---
+++ chapters/appendices/out.sh
@@ -0,0 +1,12 @@
+$ python simple_generation.py -h
+usage: simple_generation.py [-h] rows cols output
+
+Generate a file for clustering
+
+positional arguments:
+  rows        number of rows
+  cols        number of columns
+  output      output file name
+
+optional arguments:
+  -h, --help  show this help message and exit
